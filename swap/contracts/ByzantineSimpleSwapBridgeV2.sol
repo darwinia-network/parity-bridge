1ed4a52ad1780fa3f0f2a33a3d134ec81d112e04
WoeOm <1015996366@qq.com>
2020-08-18 09:52:40 +0800

    pausable swap bridge

---
+++ swap/contracts/ByzantineSimpleSwapBridgeV2.sol
@@ -0,0 +1,167 @@
+pragma solidity ^0.4.23;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "./Pausable.sol";
+
+// another one is ByzantineSimpleSwapBridge.
+contract ByzantineSimpleSwapBridge is Ownable, Pausable {
+    using SafeMath for uint256;
+
+    mapping (address => bool) public supportedTokens;
+
+    uint256 public swapCount;
+
+    uint256 public startTime;
+
+    uint256 public maxSwapAmount;
+
+    address public feeWallet;
+
+    uint256 public minFee;
+
+    uint256 public feeRatio;    // default to 100, denominator is 10000
+
+    uint256 public transitionRatio;    // default to 5000, denominator is 10000
+    uint256 public transitionDuration;  // default to 6 hours
+
+    /// Event created on initilizing token dex in source network.
+    event TokenSwapped(
+    uint256 indexed swapId, address from, bytes32 to, uint256 amount, address token, uint256 fee, uint256 srcNetwork, uint256 dstNetwork);
+
+    event ClaimedTokens(address indexed _token, address indexed _controller, uint _amount);
+
+
+    /// Constructor.
+    constructor (
+        address _feeWallet,
+        uint256 _minFee,
+        uint256 _transitionRatio,
+        uint256 _maxSwapAmount,
+        uint256 _startTime
+    ) public
+    {
+        feeWallet = _feeWallet;
+        minFee = _minFee;
+        transitionRatio = _transitionRatio;
+        maxSwapAmount = _maxSwapAmount;
+
+        startTime = _startTime;
+        
+        transitionDuration = 6*3600;
+
+        feeRatio = 100;
+    }
+
+    //users initial the exchange token with token method of "approveAndCall" in the source chain network
+    //then invoke the following function in this contract
+    //_amount include the fee token
+    function receiveApproval(address from, uint256 _amount, address _token, bytes _data) public whenNotPaused {
+
+        require(supportedTokens[_token], "Not suppoted token.");
+        require(msg.sender == _token, "Invalid msg sender for this tx.");
+
+        uint256 swapAmount;
+        uint256 dstNetwork;
+        bytes32 receipt;
+
+        assembly {
+            let ptr := mload(0x40)
+            calldatacopy(ptr, 0, calldatasize)
+            swapAmount := mload(add(ptr, 164))
+            dstNetwork := mload(add(ptr, 196))
+            receipt :=  mload(add(ptr, 228))
+        }
+
+        require(startTime == 0 || now >= startTime, "Swap should be after the start time.");
+
+        require(swapAmount <= maxSwapAmount, "Swap amount must be less than max swap amount.");
+        require(swapAmount > 0, "Swap amount must be larger than zero.");
+
+        uint256 requiredFee = querySwapFeeForNow(swapAmount);
+        require(_amount >= swapAmount.add(requiredFee), "No enough of token amount are approved.");
+
+        if(requiredFee > 0) {
+            require(ERC20(_token).transferFrom(from, feeWallet, requiredFee), "Fee transfer failed.");
+        }
+
+        require(ERC20(_token).transferFrom(from, this, swapAmount), "Swap amount transfer failed.");
+
+        emit TokenSwapped(swapCount, from, receipt, swapAmount, _token, requiredFee, 200000, dstNetwork);
+        
+        swapCount = swapCount + 1;
+    }
+
+    function addSupportedToken(address _token) public onlyOwner {
+        supportedTokens[_token] = true;
+    }
+
+    function removeSupportedToken(address _token) public onlyOwner {
+        supportedTokens[_token] = false;
+    }
+
+    function changeStartTime(uint256 _startTime) public onlyOwner {
+        startTime = _startTime;
+    }
+
+    function changeMaxSwapAmount(uint256 _maxSwapAmount) public onlyOwner {
+        maxSwapAmount = _maxSwapAmount;
+    }
+
+    function changeFeeWallet(address _newFeeWallet) public onlyOwner {
+        feeWallet = _newFeeWallet;
+    }
+
+    function changeMinFee(uint256 _minFee) public onlyOwner {
+        minFee = _minFee;
+    }
+
+    function changeFeeRatio(uint256 _feeRatio) public onlyOwner {
+        feeRatio = _feeRatio;
+    }
+
+    function changeTransitionRatio(uint256 _transitionRatio) public onlyOwner {
+        transitionRatio = _transitionRatio;
+    }
+
+    function changeTransitionDuration(uint256 _transitionDuration) public onlyOwner {
+        transitionDuration = _transitionDuration;
+    }
+
+    function querySwapFeeForNow(uint256 _amount) public view returns (uint256) {
+        return querySwapFee(_amount, now);
+    }
+
+    function querySwapFee(uint256 _amount, uint256 _time) public view returns (uint256) {
+        uint256 requiredFee = feeRatio.mul(_amount).div(10000);
+
+        if (startTime == 0 || _time >= (startTime + transitionDuration)) {
+            // do nothing.
+        } else {
+            uint256 transitionFee = transitionRatio.mul(_amount).mul(startTime.add(transitionDuration).sub(_time)).div(transitionDuration * 10000);
+            if (requiredFee < transitionFee) {
+                requiredFee = transitionFee;
+            }
+        }
+
+        if (requiredFee < minFee ) {
+            requiredFee = minFee;
+        }
+
+        return requiredFee;
+    }
+
+    function claimTokens(address _token) public onlyOwner {
+        if (_token == 0x0) {
+            address(msg.sender).transfer(address(this).balance);
+            return;
+        }
+
+        ERC20 token = ERC20(_token);
+        uint balance = token.balanceOf(this);
+        token.transfer(address(msg.sender), balance);
+
+        emit ClaimedTokens(_token, address(msg.sender), balance);
+    }
+}
---
+++ swap/contracts/Pausable.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.4.23;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+/**
+ * @title Pausable
+ * @dev Base contract which allows children to implement an emergency stop mechanism.
+ */
+contract Pausable is Ownable  {
+  event Pause();
+  event Unpause();
+
+  bool public paused = false;
+
+
+  /**
+   * @dev Modifier to make a function callable only when the contract is not paused.
+   */
+  modifier whenNotPaused() {
+    require(!paused, "Is paused");
+    _;
+  }
+
+  /**
+   * @dev Modifier to make a function callable only when the contract is paused.
+   */
+  modifier whenPaused() {
+    require(paused, "Is not paused");
+    _;
+  }
+
+  /**
+   * @dev called by the owner to pause, triggers stopped state
+   */
+  function pause() public onlyOwner whenNotPaused {
+    paused = true;
+    emit Pause();
+  }
+
+  /**
+   * @dev called by the owner to unpause, returns to normal state
+   */
+  function unpause() public onlyOwner whenPaused {
+    paused = false;
+    emit Unpause();
+  }
+}
